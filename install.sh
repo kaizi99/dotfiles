9512ad45406f18a2adcefa82f91f5f70cfb6453f
kaizi99 <kaizi99@gmx.de>
2017-08-28 00:37:12 +0200

    Init

---
+++ install.sh
@@ -0,0 +1,21 @@
+# Install yaourt
+
+mkdir temp
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+tar -xvzf package-query.tar.gz
+cd package-query
+makepkg -si
+cd ..
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+tar -xvzf yaourt.tar.gz
+cd yaourt
+makepkg -si
+cd ..
+rm -R temp
+
+# Install required programs
+
+sudo pacman -S ttf-inconsolata
+yaourt -S i3-gaps
+yaourt -S ttf-font-awesome
+yaourt -S polybar-git
